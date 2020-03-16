df52f7f08d5d6079d894aff13db27621bc5493fb
August Johnson <gusjohnson98@gmail.com>
2020-03-16 17:42:53 -0500

    react setup

---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <span>PLACEHOLDER INFORMATION</span>
+    </footer>
+  );
+}
+
+export default Footer;
