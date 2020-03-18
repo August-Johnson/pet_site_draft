46634c61b7c1bb07cc629493688bf240950f5d36
August Johnson <gusjohnson98@gmail.com>
2020-03-17 20:49:34 -0500

    created components/pages folders

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,12 +1,20 @@
 import React from "react";
-import "./style.css";
 
 function Footer() {
-  return (
-    <footer className="footer">
-      <span>PLACEHOLDER INFORMATION</span>
-    </footer>
-  );
+    return (
+        <footer className="homepage-footer">
+            <h2>Footer</h2>
+            <span>Phone: </span>651-271-6005
+            <br />
+            <span>Email: </span>gusjohnson98@gmail.com
+            <br />
+            <a href="">My GitHub</a>
+            <br />
+            <a href="">LinkedIn</a>
+            <br />
+            <a href="">Resume</a>
+        </footer>
+    );
 }
 
 export default Footer;
